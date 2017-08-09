c6de0a281e48f86761b606943f4c61bfcaacf91c
Carson Full <carsonfull@gmail.com>
2017-08-08 19:07:36 -0500

    Fix side effects sniff to continue searching for a side effect after allowed ones.

--- Bolt/Sniffs/Files/SideEffectsSniff.php
+++ Bolt/Sniffs/Files/SideEffectsSniff.php
@@ -16,32 +16,53 @@ class SideEffectsSniff implements Sniff
     {
         $tokens = $file->getTokens();
 
+        $start = 0;
+        while (true) {
+            list($symbolIndex, $effectIndex) = $this->searchForConflict($file, $start, $file->numTokens - 1, $tokens);
+
+            if ($symbolIndex === null || $effectIndex === null) {
+                $file->recordMetric($stackPtr, 'Declarations and side effects mixed', 'no');
+
+                // Ignore the rest of the file.
+                return $file->numTokens + 1;
+            }
+
+            $start = $file->findEndOfStatement($effectIndex) + 1;
+
+            $symbol = $tokens[$symbolIndex];
+            $effect = $tokens[$effectIndex];
+
+            // If line is ignored, try again at next statement
+            if (isset($file->getIgnoredLines()[$effect['line']])) {
+                continue;
+            }
+
+            // If effect is allowed, try again at next statement
+            if ($effect['code'] === T_STRING && in_array($effect['content'], ['Deprecated', 'trigger_error'])) {
+                $start = $file->findEndOfStatement($effectIndex) + 1;
+                continue;
+            }
+
+            $error = 'A file should declare new symbols (classes, functions, constants, etc.) and cause no other side effects, or it should execute logic with side effects, but should not do both. The first symbol is defined on line %s and the first side effect is on line %s.';
+            $data = [
+                $symbol['line'],
+                $effect['line'],
+            ];
+            $file->addWarning($error, $effectIndex, 'FoundWithSymbols', $data);
+            $file->recordMetric($stackPtr, 'Declarations and side effects mixed', 'yes');
+
+            return $start;
+        }
+    }
+
+    protected function searchForConflict(File $file, $start, $end, $tokens)
+    {
         $sniff = new \PHP_CodeSniffer\Standards\PSR1\Sniffs\Files\SideEffectsSniff();
         $ref = new \ReflectionMethod($sniff, 'searchForConflict');
         $ref->setAccessible(true);
-        $result = $ref->invoke($sniff, $file, 0, ($file->numTokens - 1), $tokens);
 
-        if ($result['symbol'] !== null && $result['effect'] !== null) {
-            $error = 'A file should declare new symbols (classes, functions, constants, etc.) and cause no other side effects, or it should execute logic with side effects, but should not do both. The first symbol is defined on line %s and the first side effect is on line %s.';
-            $data = [
-                $tokens[$result['symbol']]['line'],
-                $tokens[$result['effect']]['line'],
-            ];
+        $result = $ref->invoke($sniff, $file, $start, $end, $tokens);
 
-            //----- Modified from parent (allow calls to "Deprecated" and "trigger_error") --------
-            $effect = $tokens[$result['effect']];
-            if ($effect['code'] === T_STRING && in_array($effect['content'], ['Deprecated', 'trigger_error'])) {
-                return $file->numTokens + 1;
-            }
-            //----- end modification --------
-
-            $file->addWarning($error, $result['effect'], 'FoundWithSymbols', $data);
-            $file->recordMetric($stackPtr, 'Declarations and side effects mixed', 'yes');
-        } else {
-            $file->recordMetric($stackPtr, 'Declarations and side effects mixed', 'no');
-        }
-
-        // Ignore the rest of the file.
-        return ($file->numTokens + 1);
+        return [$result['symbol'], $result['effect']];
     }
 }
